8dfe52abab59d722615a3a627af8c98cf9ed664c
Szymon Lewandowski <slewandowski999@gmail.com>
2019-04-29 15:05:38 +0100

    Applied changes to the Intro Scene:

---
+++ Assets/Scripts/Other/SkipScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class SkipScene : MonoBehaviour 
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
