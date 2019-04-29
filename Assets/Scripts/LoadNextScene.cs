282b5bbc085b3019f02a47ea0e0dacac775646ca
slewandowski <slewandowski999@gmail.com>
2019-04-29 12:30:15 +0100

    Added Video Scene:
    Also updated scene management

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour {
+
+	void Start ()
+    {
+		StartCoroutine (loadSceneAfterDelay(9));
+	}
+
+	IEnumerator loadSceneAfterDelay(float waitbySecs){
+
+		yield return new WaitForSeconds(waitbySecs);
+		SceneManager.LoadScene (2);
+	} 
+}
+
+		
+	
+
+
+	
