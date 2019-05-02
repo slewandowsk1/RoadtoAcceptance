2a73e0aea4b5a5b0b915e4b5e7f2a443bc07a816
rabiul <mrabiul-islam@elam.co.uk>
2019-05-02 10:47:08 +0100

    wop

---
+++ Assets/Scripts/DoorText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorText : MonoBehaviour {
+    public Text pressE;
+
+
+	// Use this for initialization
+	void Start () {
+        pressE.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        pressE.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        pressE.gameObject.SetActive(false);
+    }
+}
