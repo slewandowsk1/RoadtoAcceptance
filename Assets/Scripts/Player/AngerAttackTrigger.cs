ffd340b48843b08f915df1359a9b2d796e3dff08
Szymon Lewandowski <slewandowski999@gmail.com>
2019-05-07 16:00:29 +0100

    Updated folder & Anger Scene

---
+++ Assets/Scripts/Player/AngerAttackTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngerAttackTrigger : MonoBehaviour
+{
+
+    public int gainHealth = 1;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.isTrigger == false && collision.CompareTag("enemy"))
+        {
+            collision.GetComponent<AngerBoss>().GainHealth(gainHealth);
+            //collision.SendMessageUpwards("Damage", dmg);
+        }
+
+    }
+}
