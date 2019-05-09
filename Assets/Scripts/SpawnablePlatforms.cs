91bac618484d248acfb26d8aa2e1205c27697862
slewandowski <slewandowski999@gmail.com>
2019-05-09 11:56:26 +0100

    Breakable platforms

---
+++ Assets/Scripts/SpawnablePlatforms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnablePlatforms : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(true);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == ("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+
+        
+    }
+    private void Update()
+    {
+        if (gameObject == false)
+        {
+            Invoke("ChangeColor", 1);
+        }
+    }
+
+    void ChangeColor()
+    {
+        gameObject.SetActive(true);
+    }
+    
+} //FAKUBOTCH
+    
